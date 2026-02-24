3649656efd9805956c8d17aaf4ff5192808a032f
Harish Kumar <a.harishkumar1203@gmail.com>
2026-02-24 09:25:30 +0530

    Updated the movie

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,7 @@ function Home(){
         {id: 2, title: "Bison" , release_date: "2025"},
         {id: 3, title: "Dragon" , release_date: "2024"},
         {id: 4, title: "LIK" , release_date: "2026"},
+        {id: 5, title: "Sarvam Maya" , release_date: "2025"}
     ];
     return <div className="home">
         <div className="movies-grid">
