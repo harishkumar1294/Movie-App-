9dc7e736be7d28025c931b83e84fd5ebccf4fdd1
Harish Kumar <a.harishkumar1203@gmail.com>
2026-02-24 11:01:59 +0530

    Added NavBar to the webpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import { Routes , Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
+    <div>
+      <NavBar />
     <main className='main-content'>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/favorites" element={<Favorites />}/>
       </Routes>
     </main>
+    </div>
     )
 }
 
