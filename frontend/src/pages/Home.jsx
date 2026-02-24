23e1b515bc9b0ca15bcbf1f62517326e73c416e6
Harish Kumar <a.harishkumar1203@gmail.com>
2026-02-24 09:00:37 +0530

    Initial Commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import MovieCard from "../components/MovieCard"
+
+function Home(){
+    const movies = [
+        {id: 1, title: "Dude" , release_date: "2025"},
+        {id: 2, title: "Bison" , release_date: "2025"},
+        {id: 3, title: "Dragon" , release_date: "2024"},
+        {id: 4, title: "LIK" , release_date: "2026"},
+    ];
+    return <div className="home">
+        <div className="movies-grid">
+            {movies.map((movie) => (
+                <MovieCard movie={movie} key={movie.id} />
+            ))}
+        </div>
+    </div>
+}
+export default Home
